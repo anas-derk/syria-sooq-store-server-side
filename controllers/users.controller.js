cd6423cd64afd7d91fb1801776e16a9c83ca56e9
anas-derk <anas.derk2023@gmail.com>
2025-02-19 17:50:44 +0300

    Fix

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -107,6 +107,7 @@ async function getForgetPassword(req, res) {
         res.json(result);
     }
     catch (err) {
+        console.log(err);
         res.status(500).json(getResponseObject(getSuitableTranslations("Internal Server Error !!", req.query.language), true, {}));
     }
 }
