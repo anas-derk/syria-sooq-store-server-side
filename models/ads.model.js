c6a9d02b441a33428913377c9bf237fff6bf7b9e
anas-derk <anas.derk2023@gmail.com>
2025-03-22 15:07:48 +0300

    Fix

--- models/ads.model.js
+++ models/ads.model.js
@@ -1,6 +1,6 @@
 // Import Product Model Object
 
-const { adsModel, adminModel } = require("../models/all.models");
+const { adsModel, adminModel, productModel } = require("../models/all.models");
 
 const { getSuitableTranslations } = require("../global/functions");
 
