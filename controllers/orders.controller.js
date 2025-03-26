529536a5363b98288bc61ab1f550da22727792d8
anas-derk <anas.derk2023@gmail.com>
2025-03-26 17:56:58 +0300

    Fix

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -116,7 +116,6 @@ async function postApprovingOnReturnProduct(req, res) {
         res.json(result);
     }
     catch (err) {
-        console.log(err);
         res.status(500).json(getResponseObject(getSuitableTranslations("Internal Server Error !!", req.query.language), true, {}));
     }
 }
