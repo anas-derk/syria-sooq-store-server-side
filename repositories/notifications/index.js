4065f6405129906602b366d16a01e8b43a2a0c80
anas derk <anas.derk@gmail.com>
2026-01-03 15:32:27 +0300

    Test

--- repositories/notifications/index.js
+++ repositories/notifications/index.js
@@ -8,6 +8,7 @@ const { getSuitableTranslations } = require("../../helpers/translation");
 
 async function registerToken(userId, token, language) {
     try {
+        console.log(token);
         const user = await userModel.findById(userId);
         if (!user) {
             return {
