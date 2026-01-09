b3d42062d0f0dd7fef7363865df083828c3c7a67
anas derk <anas.derk@gmail.com>
2026-01-09 17:30:12 +0300

    Continue

--- controllers/orders/index.js
+++ controllers/orders/index.js
@@ -113,7 +113,6 @@ async function postNewRequestToReturnOrderProducts(req, res) {
         res.json(result);
     }
     catch (err) {
-        console.log(err);
         res.status(500).json(getResponseObject(getSuitableTranslations("Internal Server Error !!", req.query.language), true, {}));
     }
 }
