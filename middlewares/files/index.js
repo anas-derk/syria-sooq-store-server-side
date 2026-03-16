4da7e9ee413835af52432c7bce1670c035e87229
anas derk <anas.derk@gmail.com>
2026-03-17 01:12:35 +0300

    Continue

--- middlewares/files/index.js
+++ middlewares/files/index.js
@@ -1,5 +1,15 @@
 const { getResponseObject } = require("../../helpers/responses");
 
+const { unlinkSync } = require("fs");
+
+let fileTypeFromFileFunc, fileTypeFromBufferFunc;
+
+(async () => {
+    const { fileTypeFromFile, fileTypeFromBuffer } = await import("file-type");
+    fileTypeFromFileFunc = fileTypeFromFile;
+    fileTypeFromBufferFunc = fileTypeFromBuffer;
+})();
+
 function validateIsExistErrorInFiles(req, res, next) {
     const uploadError = req.uploadError;
     if (uploadError) {
@@ -9,6 +19,52 @@ function validateIsExistErrorInFiles(req, res, next) {
     next();
 }
 
+async function validateRealFilesType(req, res, next) {
+    try {
+        let files = [];
+        // single
+        if (req.file) {
+            files = [req.file];
+        }
+        // array
+        else if (Array.isArray(req.files)) {
+            files = req.files;
+        }
+        // fields
+        else if (req.files && typeof req.files === "object") {
+            files = Object.values(req.files).flat();
+        }
+        if (!files.length) {
+            return res.status(400).json(
+                getResponseObject("Sorry, File is required !!", true, {})
+            );
+        }
+        for (const file of files) {
+            const fileType = req.storageType === "memory"
+                ? await fileTypeFromBufferFunc(file.buffer)
+                : await fileTypeFromFileFunc(file.path);
+
+            if (!fileType || !req.allowedMimeTypes.includes(fileType.mime)) {
+
+                if (req.storageType !== "memory" && file.path) {
+                    try {
+                        unlinkSync(file.path);
+                    } catch (_) { }
+                }
+                return res.status(400).json(
+                    getResponseObject("Sorry, Invalid Real File Type !!", true, {})
+                );
+            }
+        }
+        next();
+    } catch (err) {
+        return res.status(500).json(
+            getResponseObject("Internal Server Error When Verify Real File Type !!", true, {})
+        );
+    }
+}
+
 module.exports = {
     validateIsExistErrorInFiles,
+    validateRealFilesType
 }
